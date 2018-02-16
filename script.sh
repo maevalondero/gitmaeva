b8dc939874c25dca92c0cfec67cfd5706327f15e
Maeva Londero <maeva.londero@camptocamp.com>
2018-02-16 10:05:33 +0100

    nettoyage script

--- script.sh
+++ script.sh
@@ -1,9 +1,4 @@
-<<<<<<< HEAD
-#!/bin/bash -v
-=======
-#!/bin/bash 
->>>>>>> 33cf69767e5f0a5e20bfdda5b3c58d6f2ba78fbf
-
+#!/bin/bash  -vx
 fonction1()
 {
         for branch in $var
@@ -36,29 +31,21 @@ else
         echo "LE script ne se rouve pas dans un depot git"
 
 fi
-<<<<<<< HEAD
 var2=$(git branch | tr '$var' ' ')
 #Boucle qui dispose le ls en liste
 =======
 #mettre le ls en liste
 var2=$(git branch | tr '$var' ' ')
 #condition qui dispose le ls en liste
->>>>>>> 33cf69767e5f0a5e20bfdda5b3c58d6f2ba78fbf
 for liste in $var2
         do
                 echo " $liste"
 done
-<<<<<<< HEAD
 echo "-------------------------------------"
 echo "test"
 #while [];
 cd $liste
 fonction1 $var2
-=======
-
 echo "test"
 #while [];
 cd $liste
->>>>>>> 33cf69767e5f0a5e20bfdda5b3c58d6f2ba78fbf
-
-
